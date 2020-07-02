1bf5b0663a5f912bb14f3d7a4732ee14b644a6f4
chy <chy@git.com>
2020-07-02 11:18:20 +0800

    @RefreshScope不应放在启动类上，改为Controller类上

--- scl-eureka-client-consumer/src/main/java/com/chy/scleurekaclientconsumer/SclEurekaClientConsumerApplication.java
+++ scl-eureka-client-consumer/src/main/java/com/chy/scleurekaclientconsumer/SclEurekaClientConsumerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RefreshScope
 @EnableHystrix
 @EnableFeignClients
 @EnableDiscoveryClient
--- scl-eureka-client-consumer/src/main/java/com/chy/scleurekaclientconsumer/controller/TestController.java
+++ scl-eureka-client-consumer/src/main/java/com/chy/scleurekaclientconsumer/controller/TestController.java
@@ -3,6 +3,7 @@ package com.chy.scleurekaclientconsumer.controller;
 import com.chy.scleurekaclientconsumer.feign.TestFeign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.Map;
  * @author chy
  * @date 2020-06-15 17:59
  */
+@RefreshScope
 @RestController
 @RequestMapping("/consumer")
 public class TestController {
