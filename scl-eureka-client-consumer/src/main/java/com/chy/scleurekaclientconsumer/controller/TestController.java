fc8de302423675bb6602699bf76403953e61f73a
chy <chy@git.com>
2020-07-02 11:01:01 +0800

    集成config客户端，从配置中心拉取配置

--- scl-eureka-client-consumer/src/main/java/com/chy/scleurekaclientconsumer/controller/TestController.java
+++ scl-eureka-client-consumer/src/main/java/com/chy/scleurekaclientconsumer/controller/TestController.java
@@ -2,6 +2,7 @@ package com.chy.scleurekaclientconsumer.controller;
 
 import com.chy.scleurekaclientconsumer.feign.TestFeign;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,9 @@ public class TestController {
 
     private final TestFeign feign;
 
+    @Value("${profile}")
+    private String profile;
+
     @Autowired
     public TestController(TestFeign feign) {
         this.feign = feign;
@@ -26,6 +30,11 @@ public class TestController {
     @GetMapping("/info")
     public Map<String, String> get() {
         return this.feign.getTest();
+    }
+
+    @GetMapping("/config")
+    public String getConfig() {
+        return this.profile;
     }
 
 }
