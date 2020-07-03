8552cc07028a80d137dcba15fbee6bda5c6c35cf
chy <chy@git.com>
2020-07-03 17:01:25 +0800

    将服务配置中心添加到服务注册中心并利用Spring Cloud Bus实现自动刷新配置

---
+++ scl-config-server/src/main/java/com/chy/sclconfigserver/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.chy.sclconfigserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * WEB安全配置类
+ *
+ * @author chy
+ * @date 2020-07-03 14:59
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
